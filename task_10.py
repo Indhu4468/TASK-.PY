6e4458339aefcbd5556a15d1037cda47ea3947cd
Indhu4468 <indhumathi04042005@gmail.com>
2026-03-17 19:30:42 +0530

    Add files via upload

---
+++ task_10.py
@@ -0,0 +1,104 @@
+'''a=7
+a<<2
+print(a)
+a<<2
+a=7
+a<<2
+a=137
+a>>3'''
+
+'''a=10
+print(~a)
+print(a<<2)
+print(a>>3)'''
+
+'''a=10
+b=20
+c=35
+d=45
+print(a>b and a>d)
+print(a>b or a>d )
+print(a<b or a<c)
+print(not a<=d or d<=c)'''
+
+'''c='u'
+c=='a' or c=='e' or c=='i' or c=='o' or c=='u'
+print(c=='u')'''
+
+'''f=14
+print(f%10==7 or f%7==0)
+print(f%10==7)
+print(f%7==0)'''
+
+'''a=int(input("enter"))
+b=int(input("enter"))
+if b>a:
+    print("b is a big")'''
+
+'''FIRST 
+length=int(input("enter length"))
+breath=int(input("enter breath"))
+if length==breath:
+    print("squre")
+else:
+    print("rectangle")'''
+
+'''SECOND
+num=int(input("enter:"))
+squre=num*num
+
+if squre>50:
+    print("squre is above 50")
+else:
+    print("squre is below 50")'''
+
+'''num=int(input("enter"))
+if num%2==0:
+    print("zero")
+else:
+    print("not")'''
+
+"""THIRD
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+diffrence=a-b
+if diffrence==0:
+    print("zero")
+else:
+    print("not")"""
+
+"""four
+marks=int(input("enter"))
+if marks>50:
+    print("pass")
+else:
+    print("fail")"""
+
+'''five
+num=int(input("enter"))
+if num%10==0:
+    print("number is divisible")
+else:
+    print("number is not divisible")'''
+
+'''six
+a=int(input("enter number"))
+b=int(input("enter number"))
+if a>b:
+    print("biggest")
+else:
+    print("not biggest")'''
+
+'''seven
+value=int(input("enter value"))
+if value==1:
+    print("you can go out and play")
+else:
+    print("you cannot go out and play")'''
+    
+'''Eight
+choise=int(input("enter choise is"))
+if choise==1:
+    print("The exam will be easy")
+else:
+    print("The exam will be difficult")'''
